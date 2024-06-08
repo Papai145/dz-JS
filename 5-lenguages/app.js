3b5a018f151327d13a905ace0c470e0f9a8b3091
Papai145 <mrruslan28@gmail.com>
2024-06-08 17:06:02 +0500

    первое дз

---
+++ 5-lenguages/app.js
@@ -0,0 +1,16 @@
+let lengua = "de";
+
+switch (true) {
+  case lengua === "en":
+    console.log("Hallo");
+    break;
+  case lengua === "ru":
+    console.log("Привет");
+    break;
+  case lengua === "de":
+    console.log("Guten Tag");
+    break;
+  case lengua === "es":
+    console.log("Hola");
+    break;
+}
